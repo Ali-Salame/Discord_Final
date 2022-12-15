7c5fd579f12d9fcc638073d50e104bf5b7bf4042
Ali Salame <alisalamaxd@outlook.com>
2022-12-15 02:38:28 +0200

    Bug Fixes #2

--- src/Components/Overlay.jsx
+++ src/Components/Overlay.jsx
@@ -14,12 +14,12 @@ const Overlay = (props) => {
     }
     
   return (
-    <div className='Main_Overlay'>
-        <ul className='Main_Overlay_ul'>
-            <li className={activate ? 'Main_Overlay_ul_li' : 'Main_Overlay_ul_li0'} onClick={() => {props.next(1); props.sandwishfun(prev => !prev)}} href="#" style={current_page === 1 ? {color:'#b0fffe',textShadow: "0 0 1em #b0fffe"} : {color:'white'}}>HOME</li>
-            <li className={activate ? 'Main_Overlay_ul_li' : 'Main_Overlay_ul_li0'} onClick={() => {props.next(2); props.sandwishfun(prev => !prev)}} href="#" style={current_page === 2 ? {color:'#b0fffe',textShadow: "0 0 1em #b0fffe"} : {color:'white'}}>ABOUT US</li>
-            <li className={activate ? 'Main_Overlay_ul_li' : 'Main_Overlay_ul_li0'} onClick={() => {props.next(3); props.sandwishfun(prev => !prev)}} href="#" style={current_page === 3 ? {color:'#b0fffe',textShadow: "0 0 1em #b0fffe"} : {color:'white'}}>JOIN US</li>
-            <li className={activate ? 'Main_Overlay_ul_li' : 'Main_Overlay_ul_li0'} onClick={() => {props.next(4); props.sandwishfun(prev => !prev)}} href="#" style={current_page === 4 ? {color:'#b0fffe',textShadow: "0 0 1em #b0fffe"} : {color:'white'}}>MEMBERS</li>
+    <div className='Overlay'>
+        <ul className='Overlay_ul'>
+            <li className={activate ? 'Overlay_ul_li' : 'Overlay_ul_li0'} onClick={() => {props.next(1); props.sandwishfun(prev => !prev)}} href="#" style={current_page === 1 ? {color:'#b0fffe',textShadow: "0 0 1em #b0fffe"} : {color:'white'}}>HOME</li>
+            <li className={activate ? 'Overlay_ul_li' : 'Overlay_ul_li0'} onClick={() => {props.next(2); props.sandwishfun(prev => !prev)}} href="#" style={current_page === 2 ? {color:'#b0fffe',textShadow: "0 0 1em #b0fffe"} : {color:'white'}}>ABOUT US</li>
+            <li className={activate ? 'Overlay_ul_li' : 'Overlay_ul_li0'} onClick={() => {props.next(3); props.sandwishfun(prev => !prev)}} href="#" style={current_page === 3 ? {color:'#b0fffe',textShadow: "0 0 1em #b0fffe"} : {color:'white'}}>JOIN US</li>
+            <li className={activate ? 'Overlay_ul_li' : 'Overlay_ul_li0'} onClick={() => {props.next(4); props.sandwishfun(prev => !prev)}} href="#" style={current_page === 4 ? {color:'#b0fffe',textShadow: "0 0 1em #b0fffe"} : {color:'white'}}>MEMBERS</li>
         </ul>
     </div>
   )
